65605ab8d07d5334705c595aadfaff36dff440e5
Florestan Bredow <florestan.bredow@daiko.fr>
2016-04-18 10:29:15 +0200

    Adapte la hauteur du menu à la taille de la page

--- javascripts/sagiter.js
+++ javascripts/sagiter.js
@@ -6,12 +6,28 @@ function initMenu() {
         subMenuNav.parentNode.classList.add('dropdown');
         subMenuNav.parentNode.addEventListener("click", showSubMenu);
     }
+    window.addEventListener("resize", resizeWindow);
+    resizeWindow();
 }
 
 function showSubMenu() {
-
     this.querySelector('ul').classList.toggle('hide');
     this.querySelector('a').preventDefault();
+}
+
+function resizeWindow() {
+    var menu = document.querySelector('nav');
+    var height = (document.height !== undefined)
+        ? document.height
+        : document.body.offsetHeight;
+    var width = (document.width !== undefined)
+        ? document.width
+        : document.body.offsetWidth;
+    if (width > 768) {
+        menu.style.height = height + "px";
+    } else {
+        menu.style.height = "auto";
+    }
 }
 
 initMenu();
